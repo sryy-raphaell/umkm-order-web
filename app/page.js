bd5a226d82f67c5489fc94477b04a1920619c4f4
sryy-raphaell <srafaeltirtharamadhan@gmail.com>
2026-08-13 17:35:09 +0700

    yt cg

--- app/page.js
+++ app/page.js
@@ -1108,7 +1108,7 @@ export default function CatalogPage() {
                 </div>
                 <div className="banner-news-grid"><a
                     className="banner-news-link"
-                    href="https://youtu.be/rB1OrhkZBs4?si=G2s9hETzZdBTC_m0"
+                    href="https://youtu.be/V0D3dRtZ89I?si=XknyleAEMxPqPFUo"
                     target="_blank"
                     rel="noopener noreferrer"
                     style={{
@@ -1135,7 +1135,7 @@ export default function CatalogPage() {
                   >
                     <div>
                       <div style={{ fontWeight: 600 }}>
-                        Pengabdian kepada Masyarakat BIMA 2026 | Transformasi Digital Promosi UMKM Desa Painan 
+                        Pengabdian kepada Masyarakat BIMA 2026 | Transformasi Digital Promosi UMKM Desa Painan  
                       </div>
                       <div
                         className="banner-news-source"
